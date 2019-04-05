5c2cb7c684b4f9875c520a468f0b5718fae6fd3e
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-05 19:08:00 -0300

    Refatorar a estrutura do projeto.

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+  return render_template('user.html', name=name)
--- manage.py
+++ manage.py
@@ -1,12 +1,4 @@
-from flask import Flask
-from flask_script import Manager
-
-app = Flask(__name__)
-manager = Manager(app)
-
-@app.route('/')
-def index():
-  return 'Home'
+from app import manager
 
 if __name__ == '__main__':
   manager.run()
